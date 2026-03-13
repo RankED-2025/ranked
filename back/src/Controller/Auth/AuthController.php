c8f1aee463c3fa23cdbb6d350760e5061500f7dc
RaphaelAZ <azevedo.raphael2017@gmail.com>
2026-03-13 14:16:50 +0100

    (+) RefreshToken fixes & API docs updates

--- back/src/Controller/Auth/AuthController.php
+++ back/src/Controller/Auth/AuthController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Auth;
 
 use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
--- back/src/Controller/Auth/MeController.php
+++ back/src/Controller/Auth/MeController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Auth;
 
 use App\Entity\Eleve;
 use App\Entity\Professeur;
