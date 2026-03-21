f08c282d225009cfea2cbbb1926ffb4c911dbbe7
Félix VIART <viart.felix2017@gmail.com>
2026-03-21 13:46:20 +0100

    [RK-28] Added "rendering" tests describe for IconElement

--- front/tests/components/layouts/icon-element.test.ts
+++ front/tests/components/layouts/icon-element.test.ts
@@ -1,6 +1,6 @@
 import IconElement from '../../../src/components/layouts/IconElement.vue'
 import { mount, VueWrapper } from '@vue/test-utils'
-import { describe } from 'vitest'
+import { afterEach, describe, expect, it } from 'vitest'
 
 type IconElementProps = typeof IconElement.props
 
@@ -32,5 +32,74 @@ const mountComponent = ({
 // ------------------------------------------------------------------
 
 describe('IconElement component', () => {
+  let wrapper: VueWrapper;
 
+  afterEach(() => {
+    wrapper?.unmount()
+  })
+
+  describe('Rendering', () => {
+    it('should mount without any error', () => {
+      wrapper = mountComponent()
+      expect(true).toBe(true)
+    })
+
+    it('should render as a <i> tag', () => {
+      wrapper = mountComponent()
+      expect(wrapper.element.tagName.toLowerCase()).toBe('i')
+    })
+
+    it('should have the "icon-element" and "mdi" classes', () => {
+      wrapper = mountComponent()
+
+      const classes: DOMTokenList = wrapper.element.classList
+
+      expect(classes).toContain('icon-element')
+      expect(classes).toContain('mdi')
+    })
+
+    describe("props in the DOM", () => {
+      describe("'title' prop", () => {
+        it("should have it's 'title' prop matching the attribute 'title' in the DOM", () => {
+          wrapper = mountComponent({
+            props: {
+              title: "i'm a mocked title, yay!"
+            }
+          })
+
+          expect(wrapper.element.getAttribute('title'))
+            .toBe("i'm a mocked title, yay!")
+        })
+      })
+
+      describe("'aria-label' prop", () => {
+        it("should have it's 'ariaLabel' prop matching the attribute 'ariaLabel' in the DOM", () => {
+          wrapper = mountComponent({
+            props: {
+              ariaLabel: "i'm a mocked aria-label, yay!",
+              title: 'i should not be seen !'
+            }
+          })
+
+          expect(wrapper.element.getAttribute("aria-label"))
+            .toBe("i'm a mocked aria-label, yay!")
+        })
+
+        it.each([
+          { prop: undefined },
+          { prop: null },
+        ])('should fallback to the "title" prop when aria label is $prop', ({ prop }) => {
+          wrapper = mountComponent({
+            props: {
+              ariaLabel: prop,
+              title: 'i am the fallback title !'
+            }
+          })
+
+          expect(wrapper.element.getAttribute("aria-label"))
+            .toBe('i am the fallback title !')
+        })
+      })
+    })
+  })
 })
