7fc8a7914564562276a99ed520f3f36349060cfb
RaphaelAZ <azevedo.raphael2017@gmail.com>
2026-03-23 17:05:39 +0100

    (+) More tests

--- back/tests/Controller/Courses/Progression/ProgressionGetTest.php
+++ back/tests/Controller/Courses/Progression/ProgressionGetTest.php
@@ -12,9 +12,6 @@ class ProgressionGetTest extends WebTestCase
 {
     use ResetDatabase;
 
-    /**
-     * Test progression list retrieval with authenticated student
-     */
     public function testGetProgressionListSuccess(): void
     {
         $client = self::createClient();
@@ -53,9 +50,6 @@ class ProgressionGetTest extends WebTestCase
         $this->assertArrayHasKey('pourcentage', $responseData[0]);
     }
 
-    /**
-     * Test progression list retrieval without authentication
-     */
     public function testGetProgressionListWithoutAuthentication(): void
     {
         $client = self::createClient();
