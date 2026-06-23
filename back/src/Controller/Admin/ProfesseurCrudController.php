a9fea3ba5d7dca75d9df75e4f521d0871d95ea53
ViartFelix <viart.felix2017@gmail.com>
2026-06-23 15:18:10 +0200

    Finished Professeur easy admin CRUD

--- back/src/Controller/Admin/ProfesseurCrudController.php
+++ back/src/Controller/Admin/ProfesseurCrudController.php
@@ -3,9 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Professeur;
+use App\Form\Type\ProfesseurClassesDisplayType;
+use App\Form\Type\ProfesseurCoursDisplayType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ProfesseurCrudController extends AbstractCrudController
@@ -15,14 +19,32 @@ class ProfesseurCrudController extends AbstractCrudController
         return Professeur::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->addFormTheme('admin/form/professeur_classes_display.html.twig')
+            ->addFormTheme('admin/form/professeur_cours_display.html.twig');
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('firstname', 'Prénom'),
+            TextField::new('name', 'Nom'),
+            EmailField::new('email', 'Email'),
+
+            Field::new('classesView', 'Classes')
+                ->setFormType(ProfesseurClassesDisplayType::class)
+                ->setTemplatePath('admin/fields/professeur_classes.html.twig')
+                ->hideOnIndex()
+                ->hideWhenCreating(),
+
+            Field::new('coursView', 'Cours')
+                ->setFormType(ProfesseurCoursDisplayType::class)
+                ->setTemplatePath('admin/fields/professeur_cours.html.twig')
+                ->hideOnIndex()
+                ->hideWhenCreating(),
         ];
     }
-    */
 }
