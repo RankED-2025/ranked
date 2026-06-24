88c1024dfa72f1fe495835a39a95f3e16def2437
ViartFelix <viart.felix2017@gmail.com>
2026-06-24 14:21:25 +0200

    Added Password Reset token CRUD

--- back/src/Controller/Admin/PasswordResetTokenCrudController.php
+++ back/src/Controller/Admin/PasswordResetTokenCrudController.php
@@ -3,9 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\PasswordResetToken;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class PasswordResetTokenCrudController extends AbstractCrudController
@@ -15,14 +20,22 @@ class PasswordResetTokenCrudController extends AbstractCrudController
         return PasswordResetToken::class;
     }
 
-    /*
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->disable(Action::NEW, Action::EDIT)
+            ->add(Crud::PAGE_INDEX, Action::DETAIL);
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            Field::new('userLink', 'Utilisateur')
+                ->setTemplatePath('admin/fields/user_link.html.twig'),
+            TextField::new('token', 'Token'),
+            DateTimeField::new('expiresAt', 'Expire le'),
+            BooleanField::new('used', 'Utilisé')->renderAsSwitch(false),
         ];
     }
-    */
 }
