19fc76a9a89a805e26a360654a2d3fbebebb48cf
ViartFelix <viart.felix2017@gmail.com>
2026-06-24 13:53:12 +0200

    Finished adding relations to Activite, Contenu, Qcm, Question and Reponse CRUDs

--- back/src/Controller/Admin/ContenuCrudController.php
+++ back/src/Controller/Admin/ContenuCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Contenu;
+use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
@@ -18,11 +19,27 @@ class ContenuCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
+        $currentActiviteId = null;
+        $entity = $this->getContext()?->getEntity()->getInstance();
+        if ($entity instanceof Contenu) {
+            $currentActiviteId = $entity->getActivite()?->getId();
+        }
+
         return [
             IdField::new('id')->hideOnForm(),
             TextField::new('type', 'Type'),
             UrlField::new('url', 'URL'),
-            AssociationField::new('activite', 'Activité'),
+            AssociationField::new('activite', 'Activité')
+                ->setQueryBuilder(function (QueryBuilder $qb) use ($currentActiviteId) {
+                    $qb->leftJoin('entity.contenu', 'c')
+                        ->andWhere('entity.type = :type')
+                        ->andWhere('c.id IS NULL' . ($currentActiviteId ? ' OR entity.id = :currentActiviteId' : ''))
+                        ->setParameter('type', 'contenu');
+                    if ($currentActiviteId) {
+                        $qb->setParameter('currentActiviteId', $currentActiviteId);
+                    }
+                    return $qb;
+                }),
         ];
     }
 }
--- back/src/Controller/Admin/QcmCrudController.php
+++ back/src/Controller/Admin/QcmCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Qcm;
 use App\Form\Type\QcmQuestionsDisplayType;
+use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
@@ -25,10 +26,26 @@ class QcmCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
+        $currentActiviteId = null;
+        $entity = $this->getContext()?->getEntity()->getInstance();
+        if ($entity instanceof Qcm) {
+            $currentActiviteId = $entity->getActivite()?->getId();
+        }
+
         return [
             IdField::new('id')->hideOnForm(),
             NumberField::new('gainPts', 'Points gagnés'),
-            AssociationField::new('activite', 'Activité'),
+            AssociationField::new('activite', 'Activité')
+                ->setQueryBuilder(function (QueryBuilder $qb) use ($currentActiviteId) {
+                    $qb->leftJoin('entity.qcm', 'q')
+                        ->andWhere('entity.type = :type')
+                        ->andWhere('q.id IS NULL' . ($currentActiviteId ? ' OR entity.id = :currentActiviteId' : ''))
+                        ->setParameter('type', 'qcm');
+                    if ($currentActiviteId) {
+                        $qb->setParameter('currentActiviteId', $currentActiviteId);
+                    }
+                    return $qb;
+                }),
 
             Field::new('questionsView', 'Questions')
                 ->setFormType(QcmQuestionsDisplayType::class)
